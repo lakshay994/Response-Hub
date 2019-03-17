4196ceb03fe3a9e2ee7060a6649c2bb4f51cbf8f
Lakshay Sharma <lakshay.sharma994@gmail.com>
2019-03-17 06:22:30 -0500

    OAuth Implementation. Application can now successfully grab users' information from their google accounts

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express');
+const passport = require('passport');
+const googleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/key');
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-})
+passport.use(
+    new googleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        },
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+app.get('/auth/google', 
+        passport.authenticate('google', {
+            scope: ['profile', 'email']
+        })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
