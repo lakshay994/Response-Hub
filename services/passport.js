1cfb77dffc600f4e2f7bf92bcffaf7f1b22bae88
Lakshay Sharma <lakshay.sharma994@gmail.com>
2019-03-18 11:15:57 -0500

    Heroku proxy issue fixed. Added 'proxy' property to passport.js' google-strategy

--- services/passport.js
+++ services/passport.js
@@ -10,7 +10,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback',
+            proxy: true
         },
         (accessToken, refreshToken, profile, done) => {
             User.findOne({ googleId: profile.id})
