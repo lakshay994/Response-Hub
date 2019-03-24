5074cd67eac062907f9a8666215979f270c3661d
Lakshay Sharma <lakshay.sharma994@gmail.com>
2019-03-24 13:17:17 -0500

    Added production deployment scripts.

--- index.js
+++ index.js
@@ -24,5 +24,14 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client.build'));
+
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
